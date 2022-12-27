fda05babaec9930bab8018a7962e96ef55c6cacf
manhthe647 <manhthe647@gmail.com>
2022-12-27 17:59:16 +0700

    weather-app

---
+++ app.js
@@ -0,0 +1,61 @@
+const search=document.querySelector('.search')
+const city=document.querySelector('.city')
+const country=document.querySelector('.country')
+const value=document.querySelector('.value .number')
+const shortDesc=document.querySelector('.short-desc')
+const visibility=document.querySelector('.visibility span')
+const wind=document.querySelector('.wind span')
+const sun=document.querySelector('.sun span')
+const content=document.querySelector('.content')
+const time=document.querySelector('.time')
+const body=document.querySelector('body')
+const weather=document.querySelector('#weather')
+
+weather.setAttribute('class','cold-weather')
+
+async function changeWeatherUI(capitalValue){
+    // let capitalValue=search.value.trim()
+    let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${capitalValue}&appid=d78fd1588e1b7c0c2813576ba183a667
+    `
+    let data = await fetch(apiUrl).then(res=> res.json())
+    // console.log(data);
+       if(data.cod ==200){
+        content.classList.remove('hide')
+        city.innerText=data.name
+        country.innerText=data.sys.country
+        visibility.innerText=data.visibility + 'm'
+        wind.innerText=data.wind.speed+ 'm/s'
+        sun.innerText=data.main.humidity + "%"
+        let temp=data.main.temp -273.15
+        value.innerText=~~(data.main.temp -273.15)
+        shortDesc.innerText=data.weather[0].main
+        time.innerText = new Date().toLocaleDateString('vi')
+
+        if(temp<10){
+            body.setAttribute('class','cold')
+        }
+        else if(temp<18){
+            body.setAttribute('class','cool')
+        }
+        else if(temp<24){
+            body.setAttribute('class','warm')
+        }
+        else{
+            body.setAttribute('class','hot')
+        }
+
+    }else{
+       content.classList.add('hide')
+    }
+}
+
+
+search.addEventListener('keypress',function(e){
+    let capitalValue=search.value.trim()
+    if(e.code=="Enter"){
+        changeWeatherUI(capitalValue)
+    }
+})
+
+changeWeatherUI("ha noi")
+
